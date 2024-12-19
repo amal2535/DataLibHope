d7cdeb3c8c094e33499309fdbaa2c3d2188d81e9
Amal Maatoug <maatougamal1@gmail.com>
2024-12-19 09:44:57 +0100

    adding to github

---
+++ examples/manipulation_data.py
@@ -0,0 +1,8 @@
+from datalib import DataManipulation
+import pandas as pd
+
+# Load data
+df = pd.DataFrame({'product_id': [101, 102, None], 'price': [999.99, None, 499.99]})
+
+# Handle missing values
+cleaned_df = DataManipulation.handle_missing_values(df, method='fill', fill_value=0)
---
+++ examples/plotting.py
@@ -0,0 +1,10 @@
+from datalib import Visualization
+
+# Plot histogram
+data = [1, 2, 3, 4, 5]
+Visualization.plot_histogram(data, bins=5)
+
+# Scatter plot
+x = [1, 2, 3]
+y = [4, 5, 6]
+Visualization.plot_scatter(x, y)
